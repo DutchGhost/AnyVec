688d8b99df57bf0b14302de5e4e6af3aa34d59a9
DutchGhost <kasper199914@gmail.com>
2018-05-29 13:58:01 +0200

    transmute maybe?

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,29 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#![feature(untagged_unions)]
+
+use std::mem;
+
+union Inner<A, B, C> {
+    a: A,
+    b: B,
+    c: C,
+}
+
+pub struct AnyVec<A, B, C> {
+    inner: Vec<Inner<A, B, C>>,
+}
+
+pub trait AnyDeref<T> {
+    fn anyderef(&self) -> &T;
+}
+
+// impl <A, B, C> AnyDeref<Vec<A>> for AnyVec<A, B, C> {
+//     fn anyderef(&self) -> &Vec<A> {
+//         unsafe { mem::transmute(&self.inner) }
+//     }
+// }
+
+impl <A, B, C, T> AnyDeref<Vec<T>> for AnyVec<A, B, C> {
+    fn anyderef(&self) -> &Vec<T> {
+        unsafe { mem::transmute(&self.inner) }
     }
 }
