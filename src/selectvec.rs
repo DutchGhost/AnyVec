f13726545c2f3462efcef102758671e8e5081440
DutchGhost <kasper199914@gmail.com>
2018-05-30 21:11:50 +0200

    I dont understand!

--- src/selectvec.rs
+++ src/selectvec.rs
@@ -45,15 +45,20 @@ where
     }
 }
 
-pub struct Phantom1;
-pub struct Phantom2;
-pub struct Phantom3;
+#[derive(Debug, Ord, PartialOrd, Hash, Eq, PartialEq, Default)]
+pub struct A;
 
-pub trait PhantomTrait {}
+#[derive(Debug, Ord, PartialOrd, Hash, Eq, PartialEq, Default)]
+pub struct B;
 
-impl PhantomTrait for Phantom1 {}
-impl PhantomTrait for Phantom2 {}
-impl PhantomTrait for Phantom3 {}
+#[derive(Debug, Ord, PartialOrd, Hash, Eq, PartialEq, Default)]
+pub struct C;
+
+pub trait Selector {}
+
+impl Selector for A {}
+impl Selector for B {}
+impl Selector for C {}
 
 /// This trait can be used to 'select' a current type.
 pub trait Select<S> {
@@ -62,48 +67,30 @@ pub trait Select<S> {
     type Output;
 }
 
-impl <A, B, C> Select<Phantom1> for (A, B, C) {
-    type Output = A;
+impl <AA, BB, CC> Select<A> for (AA, BB, CC) {
+    type Output = AA;
 }
 
-impl <A, B, C> Select<Phantom2> for (A, B, C) {
+impl <AA, BB, CC> Select<B> for (AA, BB, CC) {
     type Output = B;
 }
 
-impl <A, B, C> Select<Phantom3> for (A, B, C) {
-    type Output = C;
+impl <AA, BB, CC> Select<C> for (AA, BB, CC) {
+    type Output = CC;
 }
 
 impl <A, B, C> SelectUnion<A, B, C>
-where
-    A: 'static,
-    B: 'static,
-    C: 'static,
 {
-    /// Converts self into `T`.
-    /// `T` must be one of `A`, `B` and `C`.
-    /// 
-    /// # Panic
-    /// Panics if `T` is not one of `A`, `B` and `C`.
     #[inline]
-    pub fn select<T: 'static>(self) -> T {
-        assert!(<(A, B, C)>::contains::<T>());
-
+    pub fn select<S: Selector, T>(mut self) -> T
+    where
+        (A, B, C): Select<S, Output = T>
+    {
         unsafe {
-            self.select_unchecked::<T>()
+            let t = ptr::read(&mut self as *mut _ as *mut T);
+            mem::forget(self);
+            t
         }
-    }
-
-    /// Converts self into `T`.
-    /// 
-    /// # Safety
-    /// This function does not type-check if T is one of `A`, `B` and `C`.
-    /// If `T` is not one of those, this caused Undefined Behaviour.
-    #[inline]
-    pub unsafe fn select_unchecked<T>(mut self) -> T {
-        let t = ptr::read(&mut self as *mut _ as *mut T);
-        mem::forget(self);
-        t
     }
 }
 
@@ -125,9 +112,9 @@ impl <T, A, B, C> AsMut<T> for SelectUnion<A, B, C> {
 /// 
 /// # Examples
 /// ```
-/// use anyvec::selectvec::{SelectItem, SelectItemExt};
+/// use anyvec::selectvec::{SelectItem, Selector, A, B, C};
 /// 
-/// let mut item: SelectItem<u32, u32, String, ()> = SelectItemExt::from(10);
+/// let mut item: SelectItem<u32, u32, String, ()> = SelectItem::from::<A>(10);
 /// ```
 pub struct SelectItem<T, A, B, C>
 {
@@ -135,37 +122,208 @@ pub struct SelectItem<T, A, B, C>
     marker: PhantomData<T>,
 }
 
-pub trait SelectItemExt<T, S: PhantomTrait> {
-    fn from(t: T) -> Self;
-
-    fn into(self) -> T;
-}
-
-macro_rules! impl_select_item {
-    ($phantom:ty) => (
-        impl <T, A, B, C> SelectItemExt<T, $phantom> for SelectItem<T, A, B, C>
-        where
-            (A, B, C): TypeEq<Type = SelectUnion<A, B, C>> + Select<$phantom, Output = T>
-        {
-            fn from(t: T) -> Self {
-                unsafe {
-                    let mut s = mem::uninitialized();
-                    ptr::write(&mut s as *mut _ as *mut T, t);
-                    s
-                }
-            }
-
-            fn into(mut self) -> T {
-                unsafe {
-                    let t = ptr::read(&mut self as *mut _ as *mut T);
-                    mem::forget(self);
-                    t
-                }
-            }
+impl <T, A, B, C> SelectItem<T, A, B, C> {
+    pub fn from<S: Selector>(t: T) -> Self
+    where
+        (A, B, C): Select<S, Output = T>
+    {
+        unsafe {
+            let mut s = mem::uninitialized();
+            ptr::write(&mut s as *mut _ as *mut T, t);
+            s
         }
-    )
+    }
+
+    #[inline]
+    pub fn into(mut self) -> T {
+        unsafe {
+            let t = ptr::read(&mut self as *mut _ as *mut T);
+            mem::forget(self);
+            t
+        }
+    }
+
+    #[inline]
+    pub fn into_inner(self) -> SelectUnion<A, B, C> {
+        self.data
+    }
+
+    #[inline]
+    pub fn from_inner(data: SelectUnion<A, B, C>) -> SelectItem<T, A, B, C> {
+        SelectItem {
+            data,
+            marker: PhantomData,
+        }
+    }
 }
 
-impl_select_item!(Phantom1);
-impl_select_item!(Phantom2);
-impl_select_item!(Phantom3);
+impl <T, A, B, C> AsRef<T> for SelectItem<T, A, B, C> {
+    #[inline]
+    fn as_ref(&self) -> &T {
+        self.data.as_ref()
+    }
+}
+
+impl <T, A, B, C> AsMut<T> for SelectItem<T, A, B, C> {
+    #[inline]
+    fn as_mut(&mut self) -> &mut T {
+        self.data.as_mut()
+    }
+}
+
+impl <T, A, B, C> fmt::Debug for SelectItem<T, A, B, C>
+where
+    T: fmt::Debug
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.as_ref().fmt(f)
+    }
+}
+
+/// A Vector that can hold multiple data-types, and switch to those data-types, without losing its allocated space.
+pub struct SelectVec<T, A, B, C> {
+    data: Vec<SelectUnion<A, B, C>>,
+    marker: PhantomData<T>
+}
+
+impl <T, A, B, C> SelectVec<T, A, B, C> {
+    
+    /// Creates a new empty `SelectVec<T, A, B, C>`.
+    #[inline]
+    pub fn new() -> Self {
+        SelectVec {
+            data: Vec::new(),
+            marker: PhantomData,
+        }
+    }
+
+    #[inline]
+    pub fn push<S: Selector>(&mut self, item: T)
+    where
+        (A, B, C): Select<S, Output = T>
+    {
+        self.data.push(SelectItem::from::<S>(item).into_inner());
+    }
+    
+    #[inline]
+    pub fn pop<S: Selector>(&mut self) -> Option<T>
+    where
+        (A, B, C): Select<S, Output = T>
+    {
+        self.data.pop().map(|i| i.select::<S, T>())
+    }
+
+    #[inline]
+    pub fn iter(&self) -> impl Iterator<Item = &T> {
+        self.data.iter().map(|item| unsafe { mem::transmute(item) })
+    }
+
+    #[inline]
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut T> {
+        self.data.iter_mut().map(|item| unsafe { mem::transmute(item) })
+    }
+
+    #[inline]
+    pub fn into_iter<S: Selector>(self) -> impl Iterator<Item = T>
+    where
+        (A, B, C): Select<S, Output = T>
+    {
+        self.data.into_iter().map(|item| item.select::<S, T>())
+    }
+    /// Returns a new instance of `AnyVec<U, A, B, C>` by clearing the current vector, leaving the allocated space untouched.
+    /// It returns a new `AnyVec<U, A, B, C>`, that can now hold a different data-type.
+    /// # Examples
+    /// ```
+    /// use anyvec::selectvec::{SelectVec, A, B, C};
+    /// let mut vec = SelectVec::<char, char, u8, String>::new();
+    ///
+    /// vec.push::<A>('a');
+    ///
+    /// let mut changed = vec.change_type::<C, String>();
+    ///
+    /// changed.push::<C>(String::from("foo"));
+    ///
+    /// assert_eq!(changed.pop::<C>(), Some(String::from("foo")));
+    /// ```
+    ///
+    /// # Panic
+    /// Trying to change to a datatype that is not specified at creation, is not allowed, and will result in a panic!():
+    ///
+    /// ```comptime_fail
+    /// use anyvec::selectvec{SelectVec, A, B, C};
+    /// let mut vec = SelectVec::<char, char, u8, String>::new();
+    ///
+    /// vec.push::<A>('a');
+    ///
+    /// let mut changed = vec.change_type::<B, u64>();
+    /// changed.push::<B>(10);
+    /// assert_eq!(changed.pop::<B>(), Some(10));
+    ///
+    /// ```
+    #[inline]
+    pub fn change_type<S, U>(mut self) -> SelectVec<U, A, B, C>
+    where
+        (A, B, C): Select<S, Output = U>
+    {
+        self.data.clear();
+
+        SelectVec {
+            data: self.data,
+            marker: PhantomData,
+        }
+    }
+
+    /// This function calls the closure for each element, changing the current datatype in place.
+    /// This does not allocate new space.
+    /// The new datatype must be a type specified at creation of the AnyVec, otherwise this function will panic.
+    /// # Examples
+    /// ```
+    /// use anyvec::selectvec::{SelectVec, A, B, C};
+    ///
+    /// let mut vec = SelectVec::<&str, &str, Result<u32, ()>, u32>::new();
+    ///
+    /// vec.push::<A>("10");
+    /// vec.push::<A>("20");
+    /// vec.push::<A>("30");
+    /// vec.push::<A>("40");
+    ///
+    /// let mut changed = vec.map::<Result<u32, ()>, B>(|s| s.parse::<u32>().map_err(|_| ()) );
+    ///
+    /// {
+    ///     let mut iter = changed.iter();
+    ///
+    ///     assert_eq!(iter.next(), Some(&Ok(10)));
+    ///     assert_eq!(iter.next(), Some(&Ok(20)));
+    ///     assert_eq!(iter.next(), Some(&Ok(30)));
+    ///     assert_eq!(iter.next(), Some(&Ok(40)));
+    /// }
+    ///
+    ///
+    /// ```
+    #[inline]
+    pub fn map<U, S: Selector>(self, f: impl Fn(T) -> U) -> SelectVec<U, A, B, C>
+    where
+        (A, B, C): Select<S, Output = U>
+    {
+        let SelectVec { mut data, ..} = self;
+
+        unsafe {
+            let ptr = data.as_mut_ptr();
+            let len = data.len();
+            data.set_len(0);
+
+            for i in 0..len as isize {
+                let item_ptr = ptr.offset(i);
+                let any_t: SelectItem<T, A, B, C> = SelectItem::from_inner(ptr::read(item_ptr));
+                let t: T = any_t.into();
+                let u: U = f(t);
+                let any_u: SelectItem<U, A, B, C> = SelectItem::from::<S>(u);
+                ptr::write(item_ptr, any_u.into_inner());
+            }
+
+            data.set_len(len);
+        }
+
+        SelectVec {data, marker: PhantomData}
+    }
+}
